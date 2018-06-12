a241385827dcc02bd89d0369d42f1cf3eb32880c
Nic-ilow <ilownicholas@gmail.com>
2018-06-12 16:30:13 -0300

    update

--- SfdPdeApproach.py
+++ SfdPdeApproach.py
@@ -1,6 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
-from sympy.soversl.pde import pdsolve
-from sympy import Function, diff, Eq
-from sympy.abc import x, t
+
+
+width = 256
+p = np.zeros(width)
+p0 = 10
+p[0]=p0
+
+#J = -Dp *grad(p) = -D * p'
+#dp/dt = -grad(J) = (D*p')'
+
+D0 = 2.7E5
+
+lululul
+
