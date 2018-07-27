ef7774f849c64f739c3da54d4fb2a9372be2a0b4
Nicholas Ilow <ilownicholas@gmail.com>
2018-07-27 17:07:59 -0300

    Dimensionless Working, Something wrong with N(t)

--- DimensionlessApplied.py
+++ DimensionlessApplied.py
@@ -8,22 +8,6 @@ import pickle
 import time
 
 parser = argparse.ArgumentParser(description='Command line inputs:')
-#parser.add_argument('-D','--D',default=2.7E-13)
-#parser.add_argument('-dx','--dx',default=7.0)
-#parser.add_argument('-tmax','--tmax',default=1.0)
-#parser.add_argument('-p0','--p0',default=1)
-#parser.add_argument('-arate','--arate',default=100)
-#parser.add_argument('-am','--am',default=1)
-#parser.add_argument('-pscale','--pscale',default=1000)
-#parser.add_argument('-dt','--dt',default=1e-6)
-
-#parser.add_argument('-du','--du',default=.04) ### SCALED OUT LENGTH STEP
-#parser.add_argument('-uLength','--uLength',default=17) ### SCALED OUT LENGTH OF SYSTEM
-#parser.add_argument('-dv','--dv',default=8e-7) ### SCALED OUT TIME STEP
-#parser.add_argument('-vmax','--vmax',default=0.8) ### SCALED OUT TMAX
-#parser.add_argument('-pscale','--pscale',default=1000.0)
-#parser.add_argument('-p0','--p0',default=1.0)
-#parser.add_argument('-dx','--dx',default=1e-9)
 parser.add_argument('-p0hat','--p0hat',default=1e-4)
 parser.add_argument('-dxbar','--dxbar',default=6.0)
 parser.add_argument('-xbarmax','--xbarmax',default=1800.0)
@@ -44,128 +28,98 @@ epsilon = dtbar/2
 width = np.size(xbar)       
 
 ### pTot and aTot plotting array
-tArray = np.arange(1*dtbar,tbarmax+dtbar,dtbar*10)
-
+tArray = np.exp(np.arange(0,np.log(tbarmax),dtbar))-1
+p2 = np.logspace(-2,2,5,base=4)
+pArray = np.zeros((np.size(p2) , width))        
+aArray = np.zeros_like(pArray)
+pTotArray = np.zeros( ( np.size(p2) , np.size(tArray) ) )
+aTotArray = np.zeros_like(pTotArray)
 ### Initializing counters and a timer 
 tstart = time.time()
 counter=0
 ### Array Pre-allocation
-p = np.zeros(width+1)
-p_1 = np.zeros_like(p)
-telapsed = 0
+for p0hat in p2:
+        p = np.zeros(width+1)
+        p_1 = np.zeros_like(p)
+        telapsed = 0
 
-acetyl = np.zeros(width)
-pTot = np.zeros((int(int(tbarmax/dtbar+1)/10+1)))
-aTot = np.zeros_like(pTot)
-aTotAnalytical =  np.zeros_like(aTot)
-pTotAnalytical = np.zeros_like(aTotAnalytical)
+        acetyl = np.zeros(width)
+        pTot = np.zeros(np.size(tArray))
+        aTot = np.zeros_like(pTot)
+        ### Boundary Condition
+        p[0] = p0hat
+        p[width-1] = 0
+        counter2 = 0
+       
+        while telapsed<=tbarmax: # Iterating the system of tmax amount of seconds
 
-### Boundary Condition
-p[0] = p0hat
-p[width-1] = 0
-counter2 = 0
+                if abs(tArray[counter2]-telapsed)<=epsilon:
+                        pTot[counter2] = sum((p[0:width]/p0hat)) * dxbar
+                        aTot[counter2] = sum(acetyl) * dxbar
+                        #z = xbar/np.sqrt(4*D*(telapsed))
+                        
+                        #pTotAnalytical[int(counter2/10)] = p0*np.sqrt(4*D*(telapsed)/np.pi)
+                        #pTotAnalytical[int(counter2/10)-1] = scii.quad(densityAnalytical,0,x[width-1],args=(telapsed),epsabs=1e-15)[0]
+                        #pTotAnalytical[int(counter2/10)-1] = sum(scis.erfc(z))
+                        #acetylationfit = 1 - np.exp( -p0 * arate * telapsed *( (1+2*(z**2)) * scis.erfc(z) - 2*z*np.exp(-(z**2))/np.sqrt(np.pi) ) )
+                        #aTotAnalytical[int(counter2/10)] = scii.quad(acetylAnalytical,0,xbar[width-1],args=(telapsed),epsabs=1e-15)[0] 
+                        if counter2<np.size(tArray)-1:
+                                counter2+=1
 
-while telapsed<=tbarmax: # Iterating the system of tmax amount of seconds
-        pscaler = 1+p+p**2     
-        p_1[1:width] = p[1:width] +   dtbar *  ( (p[0:width-1]/pscaler[0:width-1] + p[2:width+1]/pscaler[2:width+1] - 2*p[1:width]/pscaler[1:width]) / (dxbar**2))  
-        acetyl[0:width] = acetyl[0:width] + (1 - acetyl[0:width] ) * dtbar * p[0:width] ### NO SFD
-         
-        telapsed += dtbar                     
+                pscaler = 1+p+p**2     
+                p_1[1:width] = p[1:width] +   dtbar *  ( (p[0:width-1]/pscaler[0:width-1] + p[2:width+1]/pscaler[2:width+1] - 2*p[1:width]/pscaler[1:width]) / (dxbar**2))  
+                acetyl[0:width] = acetyl[0:width] + (1 - acetyl[0:width] ) * dtbar * p[0:width] ### NO SFD
+                 
+                telapsed += dtbar                     
 
-        #counter2+=1
+                #counter2+=1
 
-        p,p_1 = p_1,p # Updating concentration array
-        p[0] = p0hat # resetting the boundary condition
-        p[width]=p[width-1] # Closed Tube Boundary
+                p,p_1 = p_1,p # Updating concentration array
+                p[0] = p0hat # resetting the boundary condition
+                p[width]=p[width-1] # Closed Tube Boundary
+        pArray[counter,:] = p[0:width]
+        aArray[counter,:] = acetyl[0:width]
+        pTotArray[counter,:] = aTot
+        aTotArray[counter,:] = pTot
+        counter+=1
+        print((time.time()-tstart)/60)
+plt.figure(3)
+ax3 = plt.gca()
+plt.figure(4)
+ax4 = plt.gca()
+for i , value in enumerate(p2): 
+        plt.figure(1)
+        plt.scatter(xbar[1:width],pArray[i,1:width]/value,s=1,label=('p0hat = %.2e'%value))
+        plt.xlabel('Dimensionless length')
+        plt.ylabel('phat/phat0')
 
-        if (counter2%10==0):
-                pTot[int(counter2/10)] = sum(p[1:width]) * dxbar
-                aTot[int(counter2/10)] = sum(acetyl) * dxbar
-                #z = xbar/np.sqrt(4*D*(telapsed))
-                
-                #pTotAnalytical[int(counter2/10)] = p0*np.sqrt(4*D*(telapsed)/np.pi)
-                #pTotAnalytical[int(counter2/10)-1] = scii.quad(densityAnalytical,0,x[width-1],args=(telapsed),epsabs=1e-15)[0]
-                #pTotAnalytical[int(counter2/10)-1] = sum(scis.erfc(z))
-                #acetylationfit = 1 - np.exp( -p0 * arate * telapsed *( (1+2*(z**2)) * scis.erfc(z) - 2*z*np.exp(-(z**2))/np.sqrt(np.pi) ) )
-                #aTotAnalytical[int(counter2/10)] = scii.quad(acetylAnalytical,0,xbar[width-1],args=(telapsed),epsabs=1e-15)[0] 
-        counter2+=1
-        #p_2[0] = p0
-print((time.time()-tstart)/60)
-#counter+=1
-#print(telapsed)
-#tend=time.time()
-#trun = (tend-tstart)/60 #In minutes
-#print(trun)
+        plt.figure(2)
+        plt.scatter(xbar[1:width],aArray[i,1:width],s=1,label=('p0hat = %.2e'%value))
+        plt.xlabel('Dimensionless length')
+        plt.ylabel('ahat')
+        
+        plt.figure(3)
+        plt.scatter(tArray , pTotArray[i,:],s=1,label=('p0hat = %.2e'%value))
+        plt.xlabel('Dimensionless Time')
+        plt.ylabel('N(t)')
+       
+        plt.figure(4)
+        plt.scatter(tArray , aTotArray[i,:],s=1,label=('p0hat = %.2e'%value))
+        plt.xlabel('Dimensionless Time')
+        plt.ylabel('A(t)')
+       
 
-#print('residual')
-#print(pTot2[0]-pTotAnalytical[0])
-#print('max residual')
-#print(max(abs(pTot2-pTotAnalytical)))
-plt.scatter(xbar[1:width],p[1:width],s=1)
-plt.show()
-'''
-### PLOTTING
-parameterString = 'arate=%.2f \n L=%.2f \n dx=%.2f'%(arate,L,dx)
-pTotResidual = np.zeros_like(dxArray)
-#
-plt.scatter(x/L , p[0:width] , marker=',', s=2 , label='SFD') 
-plt.scatter(x/L , p_2[0:width] , marker='*' , s=2 , label='No SFD')
-plt.xlabel('x/L')
-plt.ylabel('Density')
+plt.figure(1)
 plt.legend()
-
-plt.figure()
-plt.scatter(x/L , acetyl , marker=',' , s=2 , label='SFD')
-plt.scatter(x/L , acetyl , marker='*' , s=2 , label='No SFD')
-plt.xlabel('x/L')
-plt.ylabel('Acetylation')
+plt.figure(2)
 plt.legend()
-
-
-plt.figure()
-ax=plt.gca()
-ax.set_xscale('log')
-ax.set_yscale('log')
-plt.scatter(tArray , pTot , marker=',' , s=2 , label='SFD')
-plt.scatter(tArray , pTot2 , marker='*' , s=2 , label='No SFD')
-plt.xlabel('time (s)')
-plt.ylabel('N(t)')
+plt.figure(3)
+ax3.set_xscale('log')
+ax3.set_yscale('log')
 plt.legend()
-
-
-plt.figure()
-ax=plt.gca()
-ax.set_xscale('log')
-ax.set_yscale('log')
-plt.scatter(tArray , aTot , marker=',' , s=2 , label='SFD')
-plt.scatter(tArray , aTot2 , marker='*' , s=2 , label='No SFD')
-plt.xlabel('time (s)')
-plt.ylabel('A(t)')
+plt.figure(4)
+ax4.set_xscale('log')
+ax4.set_yscale('log')
 plt.legend()
-
-#pTotRatio = 
-plt.figure()
-ax=plt.gca()
-ax.set_xscale('log')
-#ax.set_yscale('log')
-plt.scatter(tArray , pTot/pTot2 , marker=',' , s=2 , label='N(t)')
-plt.scatter(tArray , aTot/aTot2 , marker='*' , s=2 , label='A(t)')
-plt.xlabel('time (s)')
-plt.ylabel('Ratio')
-plt.title('Ratio of SFD vs No SFD')
-plt.legend()
-
-plt.figure()
-ax=plt.gca()
-ax.set_xscale('log')
-#ax.set_yscale('log')
-#plt.scatter(tArray , pTot/pTotAnalytical , marker=',' , s=2 , label='SFD over Analytical')
-plt.scatter(tArray , abs(pTot2-pTotAnalytical) , marker='*' , s=2 , label='No SFD over Analytical')
-plt.xlabel('time (s)')
-plt.ylabel('Ratio')
-plt.title('Ratio of SFD and No SFD vs Analytical')
-plt.legend()
-
 
 plt.show()
-'''
